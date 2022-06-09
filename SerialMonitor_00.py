701cdb0a46049a066751f2cda4c45c5c70972af9
ATSmithPB <97251392+ATSmithPB@users.noreply.github.com>
2022-06-09 15:43:06 +0200

    last line buffer added

--- SerialMonitor_00.py
+++ SerialMonitor_00.py
@@ -1,3 +1,4 @@
+#Imports
 import serial
 import datetime
 import os
@@ -17,66 +18,58 @@ ser.stopbits = serial.STOPBITS_ONE
 ser.xonxoff = False
 ser.rtscts = False
 ser.dsrdtr = False
-ser.timeout = 10 #Specifies a read timeout in sec
+ser.timeout = 1  #Specifies a read timeout in sec
 print("Serial Connection Made...")
 
-# #Returns last full line from Serial buffer (to read fast serial streams <3ms/ln)
-# def receiving(s):
-#     last_received = b''
-#     buffer_string = b''
-#     while True:
-#         print("Buffering...")
-#         buffer_string = buffer_string + s.read(s.inWaiting())
-#         if b'\n' in buffer_string:
-#             lines = buffer_string.split(b'\n')
-#             last_received = lines[-2]
-#             buffer_string = lines[-1]
-#             if len(lines) >= 3:
-#                 break
-#     s.close 
-#     return last_received
+#Functions
+#Returns the last full line in a serial stream
+def readLatestLine(s):
+    print("Reading Latest Line...")
+    s.flushInput()
+    latestLine = s.readline()
+    return latestLine
 
-#Recieve complete line
-ser_bytes = ser.readline()
+loopDelay = 5 #seconds
+#Main loop that uploads a serial data to InfluxDB every n seconds
+while True:
+    ser_bytes = readLatestLine(ser)
+    print("Decoding...")
+    decoded_bytes = str(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+    print(decoded_bytes)
+    serData = decoded_bytes.split(',')
+    
+    #Initialize InfluxDB client
+    token = "-V7PacrGpoiuPn0BUyThutSyoGOAAiwKEhP5YHBkNeR_ZawOzUuF2i-ywf-JopOUfZ_9rCLCJ_74mEP0xiIE4A=="
+    org = "atsmitharc@gmail.com"
+    url = "https://westeurope-1.azure.cloud2.influxdata.com"
+    client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 
-#Decode last full line from serial buffer bytes to str
-print("Decoding...")
-decoded_bytes = str(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
-print(decoded_bytes)
+    #Write to InfluxDB with point structure to bucket
+    myBucket = "Prototype_02_Sensor_Data"
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+       
+    DBpoint = (
+    Point("Session 2")
+    .tag("Loc", "55,40,56,N,12,36,15,E")
+    .field("A_00", float(serData[0]))
+    .field("T_00", float(serData[1]))
+    .field("A_01", float(serData[2]))
+    .field("T_01", float(serData[3]))
+    .field("A_02", float(serData[4]))
+    .field("T_02", float(serData[5]))
+    .field("A_03", float(serData[6]))
+    .field("T_03", float(serData[7]))
+    .field("A_04", float(serData[8]))
+    .field("T_04", float(serData[9]))
+    .field("A_05", float(serData[10]))
+    .field("T_05", float(serData[11]))
+    #.time(time.time())
+    )
+    
+    print("Uploading...")
+        
+    write_api.write(bucket = myBucket, org = org, record=DBpoint)
 
-#Discretize string
-serData = decoded_bytes.split(',')
+    print("Send Success!")
 
-# #Initialize InfluxDB client
-# token = "-V7PacrGpoiuPn0BUyThutSyoGOAAiwKEhP5YHBkNeR_ZawOzUuF2i-ywf-JopOUfZ_9rCLCJ_74mEP0xiIE4A=="
-# org = "atsmitharc@gmail.com"
-# url = "https://westeurope-1.azure.cloud2.influxdata.com"
-# client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
-# 
-# #Write to InfluxDB with point structure to bucket
-# myBucket = "Prototype_02_Sensor_Data"
-# write_api = client.write_api(write_options=SYNCHRONOUS)
-#    
-# DBpoint = (
-# Point("Session 2")
-# .tag("Loc", "55,40,56,N,12,36,15,E")
-# .field("A_00", float(serData[0]))
-# .field("T_00", float(serData[1]))
-# .field("A_01", float(serData[2]))
-# .field("T_01", float(serData[3]))
-# .field("A_02", float(serData[4]))
-# .field("T_02", float(serData[5]))
-# .field("A_03", float(serData[6]))
-# .field("T_03", float(serData[7]))
-# .field("A_04", float(serData[8]))
-# .field("T_04", float(serData[9]))
-# .field("A_05", float(serData[10]))
-# .field("T_05", float(serData[11]))
-# #.time(time.time())
-# )
-# 
-# write_api.write(bucket = myBucket, org = org, record=DBpoint)
-# 
-# print("Send Success!")
-# 
-# time.sleep(2) # run code every n second
+    time.sleep(loopDelay) # run code every n second
